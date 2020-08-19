96fc677362f3de75d6be3891da6c5236fb6c6acc
genialkartik <kartik9756@gmail.com>
2020-08-19 21:12:19 +0530

    O(n^2)

--- Assignments/PerfectSquares.cpp
+++ Assignments/PerfectSquares.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int numSquares(int n) {
-        vector<int> dp(n + 1, INT_MAX);
-        dp[0] = 0;
+        vector<int> temp(n + 1, INT_MAX);
+        temp[0] = 0;
         for (int i = 1; i <= n; i++)
         {
-            for (int j = 1; j*j <= i; j++)
-                dp[i] = min(dp[i - j * j] + 1, dp[i]);
+            for (int j = 1; j*j <= i; j++){
+              cout<<temp[i - j * j] + 1 << " - "<< temp[i] <<endl;
+              temp[i] = min(temp[i - j * j] + 1, temp[i]);
+            }
         }
-        return dp[n];
+        return temp[n];
     }
 };
