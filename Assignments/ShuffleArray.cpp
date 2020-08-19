ae41c4523728a316e11996a2538bdf0ae34fa0f0
genialkartik <kartik9756@gmail.com>
2020-08-19 22:59:37 +0530

    array shuffle

--- Assignments/ShuffleArray.cpp
+++ Assignments/ShuffleArray.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> v;
-        for (int i = 0, j = n; i < n; i++, j++)
-        {
-            v.push_back(nums[i]);
-            v.push_back(nums[j]);
+      vector<int> ar;
+        for(int i=0; i<n; i++){
+          ar.push_back(nums[i]);
+          ar.push_back(nums[n+i]);
         }
-        return v;
+      return ar;
     }
 };
