6f1dfb6249814b2acb25cc1b2757607e2076ccba
genialkartik <kartik9756@gmail.com>
2020-08-19 22:24:33 +0530

    done

--- Assignments/EquilibriumIndex.cpp
+++ Assignments/EquilibriumIndex.cpp
@@ -3,31 +3,24 @@
 
 using namespace std;
 
-int equilibrium_index(vector<int>& v)
-{
+int eqIndex(vector<int>& v){
     int sum = 0;
     for (auto element : v)
         sum += element;
-    
     int left_sum = 0;
-    for (int i = 0; i < v.size(); i++)
-    {
+    for (int i = 0; i < v.size(); i++){
         if (left_sum == sum - left_sum - v[i])
             return i;
         left_sum += v[i];
     }
     return -1;
 }
-
-int main()
-{
+int main(){
     int n;
     cin >> n;
     vector<int> v(n);
     for (int i = 0; i < n; i++)
         cin >> v[i];
-    
-    cout << equilibrium_index(v);
-
+    cout << eqIndex(v);
     return 0;
 }
